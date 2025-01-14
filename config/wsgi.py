7f46f5f043e320d85b7fbd77cbc7446cd35e08eb
Sylvain Boissel <sylvain@boissel.dev>
2025-01-14 19:06:54 +0100

    Add Cling to serve static files

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for config project.
+WSGI config for the project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
